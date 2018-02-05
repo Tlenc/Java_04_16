909c5698a98d7155439e755eddc2feb167fb5c64
Tlenc <tlenciauskas@gmail.com>
2018-02-05 14:00:26 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,35 +8,40 @@ public class Main {
         System.out.println("Įveskite žodį");
         Scanner sc = new Scanner(System.in);
         String zod = sc.nextLine();
+        int kiekis = 0;
         boolean arSkaicius = false;
         try {
             Float.parseFloat(zod);
-            arSkaicius = true;
+            arSkaicius = false;
+            System.out.println("Įvesta ne žodis");
         } catch (Exception e) {
-            if (arSkaicius == true) {
-                System.out.println("Įvesta ne skaičius");
-
-            }
-            int kiekis = 0;
-            try {
-                for (int i = 0; i < zod.length(); i++) {
-                    char raide = zod.charAt(i);
-                    if (raide == 'a' || raide == 'A' && zod.charAt(i) < zod.length()) {
-                        i++;
-                        raide = zod.charAt(i);
-                            if (raide == 'b' || raide == 'B') {
-                                kiekis++;
-                            }
-                        }
+            arSkaicius =true ;
 
 
-                    }
-
-            } catch (Exception a) {
-                System.out.println("A paskutinė raidė");
-            }
-            System.out.println("Ab raidžių kiekis yra : " + kiekis);
         }
+        if(arSkaicius == true){
+            abpaieska(zod,kiekis);
+
     }
 }
+    public static void abpaieska(String zod,int kiekis){
+        try {
+
+            for (int i = 0; i < zod.length(); i++) {
+                char raide = zod.charAt(i);
+                if (raide == 'a' || raide == 'A' && zod.charAt(i) < zod.length()) {
+                    i++;
+                    raide = zod.charAt(i);
+                    if (raide == 'b' || raide == 'B') {
+                        kiekis++;
+                    }
+                }
+            }
+        } catch (Exception a) {
+            System.out.println("A paskutinė raidė");
+        }
+        System.out.println("Ab raidžių kiekis yra : " + kiekis);
+    }
+}
+
 
